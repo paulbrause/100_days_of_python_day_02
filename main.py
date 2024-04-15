0f09361b91f659583b51d098839413603c4ce84d
paulbrause <paulbrause@googlemail.com>
2024-04-15 15:01:37 +0200

    Day Project optimized

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ total = float(input("What was the total bill?\n$"))
 percentage = int(input("What percentage tip would you like to give?\n10, 12 or 15?\n"))
 people = int(input("How many people to split the bill?\n"))
 
-proportion = round(total * (1 + percentage / 100) / people, 2)
+proportion = "{:.2f}".format(total * (1 + percentage / 100) / people)
 
 print(f"Each person should pay: ${proportion}")
